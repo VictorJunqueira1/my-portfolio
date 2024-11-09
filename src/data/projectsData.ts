2ea0a29180f64577a062e02a883535babce131f4
VictorJunqueira1 <victorjunqueira.prog@gmail.com>
2024-11-09 17:04:55 -0300

    [uptade] Atualizando projetos.

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -4,13 +4,25 @@ export const projectsData = [
         projects: [
             {
                 title: "Assistente de Estudos",
-                description: "Uma plataforma que facilita o gerenciamento de estudos, com funcionalidades que ajudam na organização e no acompanhamento do progresso acadêmico de vários módulos (Matemática, Enem, Programação, Inglês e outros).",
+                description: "Uma plataforma interativa para uso pessoal e para otimizar o gerenciamento de estudos, podendo acompanhar o progresso acadêmico em diversas áreas como Matemática, Programação e Inglês.",
                 technologies: [
-                    { name: "React.js", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
-                    { name: "Next.js", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" },
-                    { name: "TypeScript", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" },
-                    { name: "TailwindCSS", svg: "https://www.svgrepo.com/show/354431/tailwindcss-icon.svg" },
-                    { name: "Firebase", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-original.svg" }
+                    {
+                        name: "React.js",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                    },
+                    {
+                        name: "Next.js",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
+                    },
+                    {
+                        name: "TypeScript",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+                    },
+                    {name: "TailwindCSS", svg: "https://www.svgrepo.com/show/354431/tailwindcss-icon.svg"},
+                    {
+                        name: "Firebase",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-original.svg"
+                    }
                 ],
                 language: "JavaScript",
                 deployLink: "https://studyassistant.vercel.app",
@@ -18,12 +30,41 @@ export const projectsData = [
                 emoji: "📚",
             },
             {
-                title: "Calculadora de Dias Proporcionais",
-                description: "Este projeto foi desenvolvido para automatizar o cálculo proporcional de contas de água e energia. O programa agiliza o processo, garantindo precisão e eficiência (antes o processo era feito no papel e demorando 5 minutos).",
+                title: "Biblioteca UI",
+                description: "Biblioteca de componentes UI responsivos e modernos. Inclui integrações com Next.js e Nuxt.js e podendo ser utilizada também com HTML, CSS e JavaScript puro.",
                 technologies: [
-                    { name: "HTML", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
-                    { name: "CSS", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
-                    { name: "JavaScript", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+                    {
+                        name: "React.js",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                    },
+                    {
+                        name: "Next.js",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
+                    },
+                    {
+                        name: "TypeScript",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+                    },
+                    {
+                        name: "TailwindCSS",
+                        svg: "https://www.svgrepo.com/show/354431/tailwindcss-icon.svg"
+                    }
+                ],
+                language: "JavaScript",
+                deployLink: "https://innotech-ui.vercel.app",
+                githubLink: "https://github.com/VictorJunqueira1/lib-ui",
+                emoji: "📱",
+            },
+            {
+                title: "Calculadora de Dias Proporcionais",
+                description: "Projeto voltado para imobiliárias e desenvolvido para automatizar o cálculo proporcional de contas de água e energia. O programa agiliza o processo, garantindo precisão e eficiência (antes o processo era feito no papel e demorando 5 minutos).",
+                technologies: [
+                    {name: "HTML", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"},
+                    {name: "CSS", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"},
+                    {
+                        name: "JavaScript",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+                    },
                 ],
                 language: "JavaScript",
                 deployLink: "https://proportional-days-calculator.vercel.app",
@@ -34,10 +75,16 @@ export const projectsData = [
                 title: "Mario Game",
                 description: "Uma recriação interativa do clássico personagem Mario, feito com sistema de autenticação e banco de dados Firebase, permite ao usuário visualizar a sua pontuação e de outros jogadores (Ranking Global).",
                 technologies: [
-                    { name: "HTML", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
-                    { name: "CSS", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
-                    { name: "JavaScript", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
-                    { name: "Firebase", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-original.svg" }
+                    {name: "HTML", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"},
+                    {name: "CSS", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"},
+                    {
+                        name: "JavaScript",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+                    },
+                    {
+                        name: "Firebase",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-original.svg"
+                    }
                 ],
                 language: "JavaScript",
                 deployLink: "https://mariogame-victorjunqueira.vercel.app",
@@ -48,56 +95,43 @@ export const projectsData = [
                 title: "Portfólio (Home Adventure)",
                 description: "Um portfólio abrangente que destaca meu trabalho como desenvolvedor no clube Home Adventure, apresentando o clube de Desbravadores para aqueles que não conhecem, levando o projeto adiante, para conseguirem doações e reconhecimento.",
                 technologies: [
-                    { name: "React.js", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
-                    { name: "Next.js", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" },
-                    { name: "TypeScript", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" },
-                    { name: "TailwindCSS", svg: "https://www.svgrepo.com/show/354431/tailwindcss-icon.svg" }
+                    {
+                        name: "React.js",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                    },
+                    {
+                        name: "Next.js",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
+                    },
+                    {
+                        name: "TypeScript",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+                    },
+                    {name: "TailwindCSS", svg: "https://www.svgrepo.com/show/354431/tailwindcss-icon.svg"}
                 ],
                 language: "JavaScript",
                 deployLink: "https://homeadventureclube.vercel.app",
                 githubLink: "https://github.com/VictorJunqueira1/portfolio-desbravadores.git",
                 emoji: "🏕️",
             },
+        ],
+    },
+    {
+        category: "C# .NET",
+        projects: [
             {
-                title: "Portfólio TCC (ERP)",
-                description: "Um portfólio detalhado que faz a apresentação do meu TCC, um sistema ERP moderno, para a gestão de departamentos e processos internos de uma empresa.",
+                title: "Princípios SOLID",
+                description: "Projeto educativo para demonstrar e aplicar os princípios SOLID. O repositório inclui exemplos de uso correto e incorreto dos princípios.",
                 technologies: [
-                    { name: "React.js", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
-                    { name: "Next.js", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" },
-                    { name: "TypeScript", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" },
-                    { name: "TailwindCSS", svg: "https://www.svgrepo.com/show/354431/tailwindcss-icon.svg" }
+                    {
+                        name: "C# .NET",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"
+                    },
                 ],
-                language: "JavaScript",
-                deployLink: "https://innotech-erp.vercel.app",
-                githubLink: "https://github.com/VictorJunqueira1/portfolio-tcc.git",
-                emoji: "📶",
-            },
-            {
-                title: "Exercícios de React.js",
-                description: "Este é o repositório no qual aplico minhas habilidades em React.js",
-                technologies: [
-                    { name: "React.js", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
-                    { name: "Next.js", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" },
-                    { name: "TypeScript", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" },
-                    { name: "TailwindCSS", svg: "https://www.svgrepo.com/show/354431/tailwindcss-icon.svg" },
-                ],
-                language: "JavaScript",
+                language: "C# .NET",
                 deployLink: "https://example.com/",
-                githubLink: "https://github.com/VictorJunqueira1/exercises-reactjs",
-                emoji: "⚛️",
-            },
-            {
-                title: "Projetos HTML",
-                description: "Este é o repositório no qual aplico minhas habilidades em HTML, CSS e JavaScript.",
-                technologies: [
-                    { name: "HTML", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
-                    { name: "CSS", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
-                    { name: "JavaScript", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
-                ],
-                language: "JavaScript",
-                deployLink: "https://example.com/",
-                githubLink: "https://github.com/VictorJunqueira1/html-projects.git",
-                emoji: "📝",
+                githubLink: "https://github.com/VictorJunqueira1/solid-principales",
+                emoji: "📦",
             },
         ],
     },
@@ -105,27 +139,33 @@ export const projectsData = [
         category: "Python",
         projects: [
             {
+                title: "Extração de Dados PDF",
+                description: "Uma ferramenta eficiente que facilita a extração de dados relevantes de documentos PDF, permitindo análises rápidas e efetivas.",
+                technologies: [
+                    {
+                        name: "Python",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+                    },
+                ],
+                language: "Python",
+                deployLink: "https://example.com/",
+                githubLink: "https://github.com/VictorJunqueira1/pdf-parser.git",
+                emoji: "📃",
+            },
+            {
                 title: "Jogo Permutação",
                 description: "Um jogo interativo que desafia os usuários a adivinhar a combinação numérica escolhida pela máquina com vários níveis de desafios.",
                 technologies: [
-                    { name: "Python", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" },
+                    {
+                        name: "Python",
+                        svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+                    },
                 ],
                 language: "Python",
                 deployLink: "https://example.com/",
                 githubLink: "https://github.com/VictorJunqueira1/py-game-math.git",
                 emoji: "🎲",
-            },
-            {
-                title: "Extração de Dados PDF",
-                description: "Uma ferramenta eficiente que facilita a extração de dados relevantes de documentos PDF, permitindo análises rápidas e efetivas.",
-                technologies: [
-                    { name: "Python", svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" },
-                ],
-                language: "Python",
-                deployLink: "https://example.com/",
-                githubLink: "https://github.com/VictorJunqueira1/pdf-parser.git",
-                emoji: "📑",
             }
         ],
-    }
+    },
 ];
